08950c68a270e1d26c44a5ebf32d5bff555b4f0e
Emilio Barrera <barrera_emilio@yahoo.es>
2025-11-02 19:18:23 +0000

    DatabaseSettings Class and appsettings  connection with db done

---
+++ ProductCatalog/Models/DatabaseSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace ProductCatalog.Models;
+
+public class DatabaseSettings
+{
+    public string ConnectionString { get; set; } = string.Empty;
+    public string DatabaseName { get; set; } = string.Empty;
+    public string ProductsCollectionName { get; set; } = string.Empty;
+}
